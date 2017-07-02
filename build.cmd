8b2bc7457fe0a7e726017f4ecfe4599074056157
Toni <toni.giorgino@gmail.com>
2017-07-02 09:11:11 +0200

    go

---
+++ build.cmd
@@ -0,0 +1,8 @@
+ set mw=C:\msys64\usr\bin
+ set PATH=%mw%;%PATH%
+ rem dir /s %mw%
+ g++ -v
+ bash ./configure --disable-shared LDFLAGS="-static -s" --prefix=/c/projects/plumed2/plumed2-win
+ make -j2
+ make install
+ 
